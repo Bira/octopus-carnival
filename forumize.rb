8a387090794aa7e9abe3cc957f2225cc2762851f
Bira <Bira@users.noreply.github.com>
2026-01-28 15:05:42 +0000

    deploy: f5b7a964a68755b12094f6d9fe493ba217e69e4f

--- forumize.rb
+++ forumize.rb
@@ -9,6 +9,12 @@ bb_code = long_lines
             .gsub(/\*\*(.+?)\*\*/, "[b]\\1[/b]")
             .gsub(/_(.+?)_/, "[i]\\1[/i]")
 
+# Replace markdown headers with bb-code headers
+
+bb_code = bb_code
+            .gsub(/^\#\#\# (.+?)$/, '[h3]\\1[/h3]')
+            .gsub(/^\#\# (.+?)$/, '[h2]\\1[/h2]')
+
 # Write to text file
 
 File.open("forum-post.txt", "w") { |f| f.puts bb_code }
